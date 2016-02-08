d2da1db858395aeaabbd11f4e3b367c0f145a1ff
Kerim Birlik <kerim.birlik@gmail.com>
2016-02-08 11:30:48 +0100

    inventory item start

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryItem : Click {
+
+	// Use this for initialization
+	private bool isPickedUp;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+	}
+}
