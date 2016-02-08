0ea63d3411a424340ad790362b1af7660e7422bc
Kerim Birlik <kerim.birlik@gmail.com>
2016-02-08 10:04:24 +0100

    Added Click and ClickUtils

---
+++ Assets/Scripts/Interactions/Click.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]private Collider2D clickArea;
+	[SerializeField]private bool isClicked;
+	
+	// Update is called once per frame
+	protected virtual void Update ()
+	{
+		if(Input.GetMouseButton(0)) 
+		{
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if (clickArea != null)
+			{
+				if (ClickUtils.AreaContainsClick(clickArea.bounds, mousePosition))
+				{
+					OnClick();
+				}
+			}
+			else
+			{
+				Debug.Log("No area was selected");
+			}
+		}
+	}
+
+	protected virtual void OnClick() 
+	{
+		Debug.Log("clicking");
+	}
+}
---
+++ Assets/Scripts/Interactions/ClickUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ClickUtils {
+
+	public static bool AreaContainsClick(Bounds area, Vector2 mousePosition) 
+	{
+		if(Input.GetMouseButton(0))
+		{
+			if(area.Contains(mousePosition))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
