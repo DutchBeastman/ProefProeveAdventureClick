22f3dfa8030db07fc77ad4c111816c499b2b1580
Fabian <fabianverk@hotmail.com>
2016-02-08 11:31:37 +0100

    Drag added, Empty

---
+++ Assets/Scripts/Interactions/Drag/Drag.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Utils
+{
+	public class Drag : Click
+	{
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		protected override void Update()
+		{
+
+		}
+	}
+}
