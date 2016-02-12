76aec75280043da84aa6231ebe5f576e22102a1e
Fabian <fabianverk@hotmail.com>
2016-02-12 13:34:44 +0100

    gameboard.cs added

---
+++ Assets/Scripts/Level/GameBoardManager/GameBoard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBoard : MonoBehaviour {
+
+	[SerializeField] private int id;
+	public int ID
+	{
+		get
+		{
+			return id;
+		}
+	}
+
+
+}
