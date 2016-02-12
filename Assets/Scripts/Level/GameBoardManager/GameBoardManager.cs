36a167e3750962d6fd3b03503b633a3a58e21289
Fabian <fabianverk@hotmail.com>
2016-02-12 13:19:25 +0100

    Meta files

---
+++ Assets/Scripts/Level/GameBoardManager/GameBoardManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameBoardManager : MonoBehaviour {
+	[SerializeField]
+	private List<GameObject> gameBoards;
+
+	protected virtual void Update()
+	{
+			
+	}
+}
