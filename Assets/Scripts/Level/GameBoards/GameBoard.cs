b82b075f189c1dbab1e8365303ef08b147423db2
Fabian <fabianverk@hotmail.com>
2016-02-15 12:44:38 +0100

    added gameboard manager and switch script

---
+++ Assets/Scripts/Level/GameBoards/GameBoard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBoard : MonoBehaviour {
+
+	[SerializeField] private int id;
+	public int ID
+	{
+		get
+		{
+			return id;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Level/GameBoards/GameBoardSwitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+
+public class GameBoardSwitch : Click {
+	[SerializeField] private GameBoardManager manager;
+	[SerializeField] private int id;
+
+	protected void Awake()
+	{
+		manager = FindObjectOfType<GameBoardManager>();
+	}
+	protected override void OnClick()
+	{
+		base.OnClick();
+		manager.GotoGameboard(id);
+	}
+}
