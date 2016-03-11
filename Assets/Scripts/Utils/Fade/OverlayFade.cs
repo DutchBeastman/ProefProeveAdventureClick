8f4da2135824c89844d7ee1a02d22bf3fbda2552
Fabian <fabianverk@hotmail.com>
2016-03-11 12:04:16 +0100

    Fade transition added

--- Assets/Scripts/Utils/Fade/OverlayFade.cs
+++ Assets/Scripts/Utils/Fade/OverlayFade.cs
@@ -6,39 +6,67 @@ namespace Utils
 	public class OverlayFade : MonoBehaviour
 	{
 		[SerializeField]
-		private SpriteRenderer spriteRend;
+		public float timeAdd = 0.05f;
 
+		private float lerpTimer;
+
+		[SerializeField]
+		private SpriteRenderer spriteRend;
 		[SerializeField]
 		private Color32 spriteBlack = new Color32(0,0,0,255);
 		[SerializeField]
 		private Color32 spriteTransparent = new Color32(255,255,255,0);
 
 		private Color32 lerpedColor;
-
-		protected virtual void Update()
-		{
-
-		}
+		private bool fadeIn;
+		private bool fadeOut;
 
 		protected virtual void Awake()
 		{
 			spriteRend = GetComponent<SpriteRenderer>();
             spriteRend.color = spriteTransparent;
+			fadeIn = false;
+            fadeOut = true;
 
 		}
-
-		public IEnumerator FadeOut()
+		protected virtual void Update()
 		{
-			yield return new WaitForSeconds(0.1f);
-			lerpedColor = Color32.Lerp(spriteBlack , spriteTransparent , 0.1f);
-			spriteRend.color = lerpedColor;
+			
+			if (fadeOut)
+			{
+				lerpTimer += timeAdd;
+				lerpedColor = Color32.Lerp(spriteBlack , spriteTransparent , lerpTimer);
+				spriteRend.color = lerpedColor;
+				if (lerpTimer > 1)
+				{
+					fadeOut = false;
+				}
+			}
+			else if (fadeIn)
+			{
+				lerpTimer += timeAdd;
+				lerpedColor = Color32.Lerp(spriteTransparent , spriteBlack , lerpTimer);
+				spriteRend.color = lerpedColor;
+				if (lerpTimer > 1)
+				{
+					fadeIn = false;
+				}
+			}
 		}
-		public IEnumerator FadeIn()
+
+		public void FadeOut()
 		{
-			yield return new WaitForSeconds(0.1f);
-			lerpedColor = Color32.Lerp(spriteTransparent , spriteBlack , 0.5f);
-			spriteRend.color = lerpedColor;
-			Debug.Log("jdalf");
+			fadeIn = false;
+			fadeOut = true;
+			lerpTimer = 0;
+			Debug.Log("FadeOut");
+		}
+		public void FadeIn()
+		{
+			fadeOut = false;
+			fadeIn = true;
+			lerpTimer = 0;
+			Debug.Log("FadeIn");
 		}
 	}
 }
