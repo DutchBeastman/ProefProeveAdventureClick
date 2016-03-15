8a305d1663ed4d0b52759a9677472f93665bf1ef
Fabian <fabianverk@hotmail.com>
2016-03-15 16:25:05 +0100

    Cursor changer added

---
+++ Assets/Scripts/Cursor/CursorChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public enum Cursor
+	{
+		Door,
+		Normal,
+		Grab,
+		Magnify
+	}
+
+public class CursorChanger : MonoBehaviour {
+
+	public Cursor cursor;
+
+
+	void OnMouseEnter()
+	{
+		
+	}
+	void OnMouseOver()
+	{
+		
+	}
+	void OnMouseExit()
+	{
+		
+	}
+}
