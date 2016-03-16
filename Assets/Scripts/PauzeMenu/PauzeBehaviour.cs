de73e7768e36478972f9cc9ce2ae495846ab8680
kerim birlik <kerim.birlik@gmail.com>
2016-03-16 23:44:06 +0100

    PauzeBehaviour can disable and enable colliders in the scene that are active.

---
+++ Assets/Scripts/PauzeMenu/PauzeBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauzeBehaviour : MonoBehaviour {
+
+	public Collider colliderz;
+	public Collider2D[] colliders;
+
+	/// <summary>
+	/// Finds all colliders active in the scene and disables them.
+	/// </summary>
+	public void OnPauze()
+	{
+		colliders = FindObjectsOfType<Collider2D>() as Collider2D[];
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			colliders[i].enabled = false;
+		}
+	}
+
+	/// <summary>
+	/// enables the found colliders in the scene.
+	/// </summary>
+	public void OnResume()
+	{
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			colliders[i].enabled = true;
+		}
+	}
+
+}
