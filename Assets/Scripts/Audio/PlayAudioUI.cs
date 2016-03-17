7bffa44a1b0f98861eda41bcae81cda39dee1070
Fabian <fabianverk@hotmail.com>
2016-03-17 14:14:29 +0100

    PlayAudio on UI added

---
+++ Assets/Scripts/Audio/PlayAudioUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+
+public class PlayAudioUI : MonoBehaviour {
+
+	private AudioAsset audioAsset;
+
+	private AudioManager audioManager;
+
+	protected virtual void Awake()
+	{
+		audioManager = GameDependencies.Get<AudioManager>();
+	}
+
+	public void PlayAudio(AudioAsset audio)
+	{
+		audioManager.Play(audio);
+		Debug.Log(audioAsset);
+	}
+}
