53975594103cbd85c6f25005e77de2f179fd9e54
Fabian <fabianverk@hotmail.com>
2016-03-18 13:47:16 +0100

    Added cursor Behaviour and event

---
+++ Assets/Scripts/Utils/IngameEvents/CursorEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Utils
+{
+	public class CursorEvent : IEvent
+	{
+
+
+		public CursorState currentcursorstate;
+		public CursorEvent(CursorState state)
+		{
+			currentcursorstate = state;
+		}
+	}
+}
