af8c3fd754d7da753dde95260eb429dfd4419356
Fabian Verkuijlen <FabianVerk@hotmail.com>
2016-03-21 00:18:20 +0100

    Added ClickableRecipe Events

---
+++ Assets/Scripts/InGame/ClickableRecipePiece.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+public class ClickableRecipePiece : ClickableItem {
+    
+    protected override void OnClick()
+    {
+        base.OnClick();
+        GlobalEvents.Invoke<RecipeEvent>();
+    }
+}
