62020ff545b610bf3a8d0a7ba34d58a36b37dd4e
Fabian Verkuijlen <FabianVerk@hotmail.com>
2016-03-21 00:17:46 +0100

    Moved CursorBehaviour

---
+++ Assets/Scripts/Cursor/CursorBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+public class CursorBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	private Texture2D doorTexture;
+	[SerializeField]
+	private Texture2D grabTexture;
+	[SerializeField]
+	private Texture2D magnifyTexture;
+	[SerializeField]
+	private Texture2D normalTexture;
+
+	protected void OnEnable()
+	{
+		GlobalEvents.AddListener<CursorEvent>(OnCursorEvent);
+	}
+
+	protected void OnDisable()
+	{
+		GlobalEvents.RemoveListener<CursorEvent>(OnCursorEvent);
+	}
+
+	/// <summary>
+	/// checks the event state and resumes or pauzes accordingly.
+	/// </summary>
+	/// <param name="evt">event sent</param>
+	public void OnCursorEvent(CursorEvent evt)
+	{
+		switch (evt.currentcursorstate)
+		{
+			case CursorState.Door:
+				Cursor.SetCursor(doorTexture , Vector2.zero , CursorMode.Auto);
+				break;
+			case CursorState.Grab:
+				Cursor.SetCursor(grabTexture , Vector2.zero , CursorMode.Auto);
+				break;
+			case CursorState.Magnify:
+				Cursor.SetCursor(magnifyTexture , Vector2.zero , CursorMode.Auto);
+				break;
+			case CursorState.Normal:
+				Cursor.SetCursor(normalTexture , Vector2.zero , CursorMode.Auto);
+				break;
+			default:
+				Cursor.SetCursor(normalTexture , Vector2.zero , CursorMode.Auto);
+				break;
+		}
+	}
+}
